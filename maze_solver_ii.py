2fbfdb8cbc17a90fc185fc25f68d09f44f2f44f4
Incinemator <giannakzorz@gmail.com>
2024-02-22 15:24:41 +0100

    Changed initialization of self.distance to float

--- maze_solver_ii.py
+++ maze_solver_ii.py
@@ -8,7 +8,7 @@ class Maze_solver:
     def __init__(self, lspeed, t):
         self.robotcontrol = RobotControl()
         self.single_distance = 0
-        self.distance = [None] * 720
+        self.distance = [float] * 720
         self.linear_time = t
         self.linear_speed = lspeed
         self.average_right = 0
